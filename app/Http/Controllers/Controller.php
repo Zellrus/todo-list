f8606e1092d9182e3cc09fb52809621e12d677c9
zellrus <aleksei.shvec96@gmail.com>
2025-10-01 16:02:27 +0300

    add documentation for api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,6 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="To-Do List API",
+ *     description="Документация для API"
+ * )
+ *
+ *
+ * @OA\Server(
+ *     url="/api/",
+ *     description="Local server"
+ * )
+ */
 
 abstract class Controller
 {
